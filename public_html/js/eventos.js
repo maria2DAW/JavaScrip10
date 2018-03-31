a2125ca5a07c38f8fb1aecb9dea934f153167f56
Maria <Maria@192.168.1.49>
2018-03-31 14:34:59 +0200

    Eventos en JS. Tipos de asignación de eventos. Flujo de burbuja.

---
+++ public_html/js/eventos.js
@@ -0,0 +1,52 @@
+/*
+ * 3 formas de ejecutar un evento:
+ * 
+    * -Como atributo de un tag HTML
+    * 
+        * <p id="parrafo" onclick="hola()">
+                   Esto es un párrafo
+          </p>
+ * 
+ *    -Como manejador semántico
+ *        obejoto.onevento = función
+ *        
+ *        document.querySelector('#parrafo').onclick = hola;
+ * 
+ *    -Como manejadores multiples
+ *        obejor.addEventListener('evento', función, boolean (default=false)); 
+ *        obejor.removeEventListener('evento', función, boolean (default=false));
+ *        
+ *        document.querySelector('#parrafo').addEventListener('click', hola);
+ *        document.querySelector('#parrafo').removeEventListener('click', hola);  
+ */
+
+/*
+ * 
+ * Encadenamiento de eventos en JS:
+ * -Por defecto van desde el elemento más interno hasta el elemento más externo (elemento padre).
+ *  -Es lo que se conoce como flujo de burbuja
+ * 
+ */
+
+var divs = document.querySelectorAll('div');
+
+function hola(evento)
+{
+    this.style.backgroundColor = 'yellow';
+    alert("Hola, te saluda " + this.className + '\nEl click lo originó ' 
+            + evento.target.className);
+    
+    console.log(evento);
+    
+    this.style.backgroundColor = '';
+    
+    //document.querySelector('#parrafo').removeEventListener('click', hola);
+}
+
+for(var i = 0; i < divs.length; i++)
+{
+    divs[i].addEventListener('click', hola)
+}
+
+
+
